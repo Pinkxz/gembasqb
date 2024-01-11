87aedf1f6053686922100022a52f5493af0d2d54
pinkxz <victor666157@gmail.com>
2024-01-11 15:33:21 -0300

    Metodos GetOne e GetAll

--- src/main/java/com/web_project/gembasqb/controllers/CompanyController.java
+++ src/main/java/com/web_project/gembasqb/controllers/CompanyController.java
@@ -1,5 +1,9 @@
 package com.web_project.gembasqb.controllers;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,7 +18,8 @@ import jakarta.validation.Valid;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 @RestController
@@ -29,5 +34,20 @@ public class CompanyController {
       BeanUtils.copyProperties(companyRDto, companyModel);
        return ResponseEntity.status(HttpStatus.CREATED).body(companyRepository.save(companyModel));
    }
+
+   @GetMapping("/companys")
+   public ResponseEntity<List<CompanyModel>> getAlCompanys() {
+       return ResponseEntity.status(HttpStatus.OK).body(companyRepository.findAll());
+   }
+   
+   @GetMapping("/companys/{id}")
+   public ResponseEntity<Object> getOneCompany(@PathVariable(value="id") UUID id) {
+      Optional<CompanyModel> company0 = companyRepository.findById(id);
+      if(company0.isEmpty()){
+         return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Company not Found");
+      }
+      return ResponseEntity.status(HttpStatus.OK).body(company0.get());
+   }
+   
 
 }
--- src/main/java/com/web_project/gembasqb/controllers/UserController.java
+++ src/main/java/com/web_project/gembasqb/controllers/UserController.java
@@ -1,9 +1,15 @@
 package com.web_project.gembasqb.controllers;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,4 +34,18 @@ public class UserController {
        return ResponseEntity.status(HttpStatus.CREATED).body(userRepository.save(userModel));
    }
 
+   @GetMapping("/users")
+   public ResponseEntity<List<UserModel>> getAllUsers() {
+       return ResponseEntity.status(HttpStatus.OK).body(userRepository.findAll());
+   }
+
+      @GetMapping("/users/{id}")
+   public ResponseEntity<Object> getOneUser(@PathVariable(value="id") UUID id) {
+      Optional<UserModel> user0 = userRepository.findById(id);
+      if(user0.isEmpty()){
+         return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not Found");
+      }
+      return ResponseEntity.status(HttpStatus.OK).body(user0.get());
+   }
+   
 }
