4069af0b794a8b8e1af8b87047877e75c8e19834
pinkxz <victor666157@gmail.com>
2024-02-24 15:50:23 -0300

    removendo importacoes

--- src/main/java/com/web_project/gembasqb/models/ServicosModel.java
+++ src/main/java/com/web_project/gembasqb/models/ServicosModel.java
@@ -1,14 +1,10 @@
 package com.web_project.gembasqb.models;
 
 import java.io.Serializable;
-import java.util.UUID;
 import org.springframework.hateoas.RepresentationModel;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 
