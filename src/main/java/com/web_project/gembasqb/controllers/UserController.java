f9ddd0c8d2aa65af1ef19ec40eab8a38fc7c5ab3
pinkxz <victor666157@gmail.com>
2024-02-29 13:04:01 -0300

    login

--- src/main/java/com/web_project/gembasqb/controllers/UserController.java
+++ src/main/java/com/web_project/gembasqb/controllers/UserController.java
@@ -1,5 +1,4 @@
 package com.web_project.gembasqb.controllers;
-
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -19,13 +18,11 @@ import org.springframework.web.bind.annotation.RestController;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 
-
 import com.web_project.gembasqb.dtos.UserRDto;
 import com.web_project.gembasqb.models.UserModel;
 import com.web_project.gembasqb.repositories.UserRepository;
 
 import jakarta.validation.Valid;
-
 
 @RestController
 public class UserController {
@@ -33,54 +30,74 @@ public class UserController {
     @Autowired
     UserRepository userRepository;
 
-   @PostMapping("/users")
-   public ResponseEntity<UserModel> saveUser(@RequestBody @Valid UserRDto userRDto) {
-      var userModel = new UserModel();
-      BeanUtils.copyProperties(userRDto, userModel);
-       return ResponseEntity.status(HttpStatus.CREATED).body(userRepository.save(userModel));
-   }
+    @PostMapping("/users")
+    public ResponseEntity<UserModel> saveUser(@RequestBody @Valid UserRDto userRDto) {
+        var userModel = new UserModel();
+        BeanUtils.copyProperties(userRDto, userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userRepository.save(userModel));
+    }
 
-   @GetMapping("/users/")
-	public ResponseEntity<List<UserModel>> getAllUsers(){
-		List<UserModel> userList = userRepository.findAll();
-		if(!userList.isEmpty()) {
-			for(UserModel user : userList) {
-				UUID id = user.getIdUser();
-				user.add(linkTo(methodOn(UserController.class).getOneUser(id)).withSelfRel());
+    @GetMapping("/users/")
+    public ResponseEntity<List<UserModel>> getAllUsers() {
+        List<UserModel> userList = userRepository.findAll();
+        if (!userList.isEmpty()) {
+            for (UserModel user : userList) {
+                UUID id = user.getIdUser();
+                user.add(linkTo(methodOn(UserController.class).getOneUser(id)).withSelfRel());
+            }
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(userList);
+    }
+
+    @GetMapping("/users/{id}")
+    public ResponseEntity<Object> getOneUser(@PathVariable(value = "id") UUID id) {
+        Optional<UserModel> userO = userRepository.findById(id);
+        if (userO.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found.");
+        }
+        userO.get().add(linkTo(methodOn(UserController.class).getAllUsers()).withRel("User List"));
+        return ResponseEntity.status(HttpStatus.OK).body(userO.get());
+    }
+
+    @DeleteMapping("/users/{id}")
+    public ResponseEntity<Object> deleteUser(@PathVariable(value = "id") UUID id) {
+        Optional<UserModel> userO = userRepository.findById(id);
+        if (userO.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found.");
+        }
+        userRepository.delete(userO.get());
+        return ResponseEntity.status(HttpStatus.OK).body("User deleted successfully.");
+    }
+
+    @PutMapping("/users/{id}")
+    public ResponseEntity<Object> updateUser(@PathVariable(value = "id") UUID id,
+            @RequestBody @Valid UserRDto userRDto) {
+        Optional<UserModel> user0 = userRepository.findById(id);
+        if (user0.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found.");
+        }
+        var userModel = user0.get();
+        BeanUtils.copyProperties(userRDto, userModel);
+        return ResponseEntity.status(HttpStatus.OK).body(userRepository.save(userModel));
+    }
+
+	@PostMapping("/login")
+	public ResponseEntity<String> login(@RequestBody UserModel userModel) {
+		String email = userModel.getEmail();
+		String password = userModel.getPassword();
+	
+		// Verificar no banco de dados se o usuário existe e se a senha está correta
+		Optional<UserModel> userOptional = userRepository.findByEmail(email);
+		if (userOptional.isPresent()) {
+			UserModel user = userOptional.get();
+			if (password.equals(user.getPassword())) {
+				// Login bem-sucedido
+				return ResponseEntity.ok("Login bem-sucedido!");
 			}
 		}
-		return ResponseEntity.status(HttpStatus.OK).body(userList);
+	
+		// Se não encontrou o usuário ou a senha está incorreta
+		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Credenciais inválidas!");
 	}
 
-	@GetMapping("/users/{id}")
-	public ResponseEntity<Object> getOneUser(@PathVariable(value="id") UUID id){
-		Optional<UserModel> userO = userRepository.findById(id);
-		if(userO.isEmpty()) {
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found.");
-		}
-		userO.get().add(linkTo(methodOn(UserController.class).getAllUsers()).withRel("User List"));
-		return ResponseEntity.status(HttpStatus.OK).body(userO.get());
-	}
-   
-   @DeleteMapping("/users/{id}")
-	public ResponseEntity<Object> deleteUser(@PathVariable(value="id") UUID id) {
-		Optional<UserModel> userO = userRepository.findById(id);
-		if(userO.isEmpty()) {
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found.");
-		}
-		userRepository.delete(userO.get());
-		return ResponseEntity.status(HttpStatus.OK).body("User deleted successfully.");
-	}
-	
-	@PutMapping("/users/{id}")
-	public ResponseEntity<Object> updateUser(@PathVariable(value="id") UUID id,
-													  @RequestBody @Valid UserRDto userRDto) {
-		Optional<UserModel> user0 = userRepository.findById(id);
-		if(user0.isEmpty()) {
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found.");
-		}
-		var userModel = user0.get();
-		BeanUtils.copyProperties(userRDto, userModel);
-		return ResponseEntity.status(HttpStatus.OK).body(userRepository.save(userModel));
-	}
 }
--- src/main/java/com/web_project/gembasqb/repositories/UserRepository.java
+++ src/main/java/com/web_project/gembasqb/repositories/UserRepository.java
@@ -1,5 +1,6 @@
 package com.web_project.gembasqb.repositories;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,5 +10,7 @@ import com.web_project.gembasqb.models.UserModel;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserModel, UUID>{
+
+    Optional<UserModel> findByEmail(String email);
     
 }
